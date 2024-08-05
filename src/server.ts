e0ae46a33c82137d4e7f8032266c098d3400b2c4
Jackson <jackson1712@hotmail.com>
2024-08-05 19:12:15 -0300

    feat: add new routes

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,10 @@ import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
 
 const app = fastify()
 
@@ -27,6 +31,10 @@ app.register(getActivities)
 app.register(createLink)
 app.register(getLinks)
 app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
 
 const port = 3333
 
